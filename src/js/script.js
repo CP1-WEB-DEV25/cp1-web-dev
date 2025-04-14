a2b54598d8b20e477a50bfdbf0ac14eca0969975
MateusAmaralFranze <amaralmateusdev@gmail.com>
2025-04-14 17:46:11 -0300

    Exercicio 2

--- src/js/script.js
+++ src/js/script.js
@@ -8,16 +8,47 @@ console.log(variavel2)
 
 let variavel3 = 40;
 console.log(variavel3)
-
-
-
-
-
 //Exercício 2:
 
+const valor = 1;
+const valor2 = 5;
 
-
+console.log(valor != valor2)
+console.log(valor == valor2)
+console.log(valor >= valor2)
 //Exercício 3:
+
+let peso = parseFloat(prompt("Qual seu peso?"));
+let altura = parseFloat(prompt("Qual sua altura?"));
+
+let imc = peso / (altura * altura);
+let faixa = "";
+
+if (imc < 18.5) {
+    faixa = "baixo";
+} else if (imc >= 18.5 && imc <= 24.9) {
+    faixa = "ideal";
+} else {
+    faixa = "alto";
+}
+
+switch (faixa) {
+    case "baixo":
+        console.log("Abaixo do peso.");
+        break;
+    case "ideal":
+        console.log("Peso ideal.");
+        break;
+    case "alto":
+        console.log("Acima do peso.");
+        break;
+}
+
+
+
+
+
+
 //Exercício 4:
 //Exercício 5:
 //Exercício 6:
