c7fc34cb1d47a322ea74e38107cd5a106e277ef6
Henrique <hlm_2005@outlook.com>
2025-04-14 19:59:18 -0300

    CP1-Web finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,78 +1,80 @@
 //Exercício 1:
 
-// let variavel;
-// console.log(variavel)
+let variavel;
+console.log(variavel)
 
-// let variavel2 = null;
-// console.log(variavel2)
+let variavel2 = null;
+console.log(variavel2)
 
-// let variavel3 = 40;
-// console.log(variavel3)
-// //Exercício 2:
+let variavel3 = 40;
+console.log(variavel3)
+//Exercício 2:
 
-// const valor = 1;
-// const valor2 = 5;
+const valor = 1;
+const valor2 = 5;
 
-// console.log(valor != valor2)
-// console.log(valor == valor2)
-// console.log(valor >= valor2)
-//Exercício 3:
-// let peso = parseFloat(prompt("Qual seu peso?"));
-// let altura = parseFloat(prompt("Qual sua altura?"));
+console.log(valor != valor2)
+console.log(valor == valor2)
+console.log(valor >= valor2)
+// Exercício 3:
+let peso = parseFloat(prompt("Qual seu peso?"));
+let altura = parseFloat(prompt("Qual sua altura?"));
 
-// let imc = peso / (altura * altura);
-// let faixa = "";
+let imc = peso / (altura * altura);
+let faixa = "";
 
-// if (imc < 18.5) {
-//     faixa = "baixo";
-// } else if (imc >= 18.5 && imc <= 24.9) {
-//     faixa = "ideal";
-// } else {
-//     faixa = "alto";
-// }
+if (imc < 18.5) {
+    faixa = "baixo";
+} else if (imc >= 18.5 && imc <= 24.9) {
+    faixa = "ideal";
+} else {
+    faixa = "alto";
+}
 
-// switch (faixa) {
-//     case "baixo":
-//         console.log("Abaixo do peso.");
-//         break;
-//     case "ideal":
-//         console.log("Peso ideal.");
-//         break;
-//     case "alto":
-//         console.log("Acima do peso.");
-//         break;
-// }
+switch (faixa) {
+    case "baixo":
+        console.log("Abaixo do peso.");
+        break;
+    case "ideal":
+        console.log("Peso ideal.");
+        break;
+    case "alto":
+        console.log("Acima do peso.");
+        break;
+}
 
 //Exercício 4:
-// let exclamacao = "!"
-// for(let exclamacao = 0; exclamacao <= 50; exclamacao ++){
-//     console.log("!", exclamacao)
-// }
+
+let exclamacao = "!"
+for(let exclamacao = 0; exclamacao <= 50; exclamacao ++){
+    console.log("!", exclamacao)
+}
 
 //Exercício 5:
 
-// let senha = 1234
-// let senhacerta = parseInt(prompt("Digite a senha de admin: "))
-// if(senhacerta == senha)
-//     alert("login realizado com sucesso!")
+let senha = 1234
+let senhacerta = parseInt(prompt("Digite a senha de admin: "))
+if(senhacerta == senha)
+    alert("login realizado com sucesso!")
+
 //Exercício 6:
 
-// let nota1 = 6.5
-// let nota2 = 7
-// let nota3 = 5.4
-// let nota4 = 5.6
-// let nota5 = 8.9
-// let nota6 = 0
-// let nota7 = 4.2
-// let media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7) / 7
-// let minima = media >= 6 ? "Passou": "Não passou";
-// console.log(minima);
-// if (media >= 6){
-//     alert("Você passou!")
-// }
-// else{
-//     alert("Você não passou.")
-// }
+let nota1 = 6.5
+let nota2 = 7
+let nota3 = 5.4
+let nota4 = 5.6
+let nota5 = 8.9
+let nota6 = 0
+let nota7 = 4.2
+let media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7) / 7
+let minima = media >= 6 ? "Passou": "Não passou";
+console.log(minima);
+if (media >= 6){
+    alert("Você passou!")
+}
+else{
+    alert("Você não passou.")
+}
 
 //Exercício 7:
 const nome = "Henrique";
@@ -106,6 +108,6 @@ console.log(info.slice(27,34))
 console.log(info.slice(92, 99))
 
 //Exercício 9:
-// let numString ="3123.223";
-// console.log(parseFloat(numString))
-// console.log(typeof numString)
+let numString ="3123.223";
+console.log(parseFloat(numString))
+console.log(typeof numString)
