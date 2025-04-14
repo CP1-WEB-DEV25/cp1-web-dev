7e6a95d377c84136ed077da18e07428f53bc5294
Felipe <ffsumida@gmail.com>
2025-04-14 18:19:17 -0300

    Exercicio 4

--- src/js/script.js
+++ src/js/script.js
@@ -1,49 +1,54 @@
 //Exercício 1:
 
-let variavel;
-console.log(variavel)
+// let variavel;
+// console.log(variavel)
 
-let variavel2 = null;
-console.log(variavel2)
+// let variavel2 = null;
+// console.log(variavel2)
 
-let variavel3 = 40;
-console.log(variavel3)
-//Exercício 2:
+// let variavel3 = 40;
+// console.log(variavel3)
+// //Exercício 2:
 
-const valor = 1;
-const valor2 = 5;
+// const valor = 1;
+// const valor2 = 5;
 
-console.log(valor != valor2)
-console.log(valor == valor2)
-console.log(valor >= valor2)
+// console.log(valor != valor2)
+// console.log(valor == valor2)
+// console.log(valor >= valor2)
 //Exercício 3:
-let peso = parseFloat(prompt("Qual seu peso?"));
-let altura = parseFloat(prompt("Qual sua altura?"));
+// let peso = parseFloat(prompt("Qual seu peso?"));
+// let altura = parseFloat(prompt("Qual sua altura?"));
 
-let imc = peso / (altura * altura);
-let faixa = "";
+// let imc = peso / (altura * altura);
+// let faixa = "";
 
-if (imc < 18.5) {
-    faixa = "baixo";
-} else if (imc >= 18.5 && imc <= 24.9) {
-    faixa = "ideal";
-} else {
-    faixa = "alto";
-}
+// if (imc < 18.5) {
+//     faixa = "baixo";
+// } else if (imc >= 18.5 && imc <= 24.9) {
+//     faixa = "ideal";
+// } else {
+//     faixa = "alto";
+// }
 
-switch (faixa) {
-    case "baixo":
-        console.log("Abaixo do peso.");
-        break;
-    case "ideal":
-        console.log("Peso ideal.");
-        break;
-    case "alto":
-        console.log("Acima do peso.");
-        break;
-}
+// switch (faixa) {
+//     case "baixo":
+//         console.log("Abaixo do peso.");
+//         break;
+//     case "ideal":
+//         console.log("Peso ideal.");
+//         break;
+//     case "alto":
+//         console.log("Acima do peso.");
+//         break;
+// }
 
 //Exercício 4:
+let exclamacao = "!"
+for(let exclamacao = 0; exclamacao <= 50; exclamacao ++){
+    console.log("!", exclamacao)
+}
+
 //Exercício 5:
 //Exercício 6:
 //Exercício 7:
