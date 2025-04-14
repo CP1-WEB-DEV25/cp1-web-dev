7bf2f2bff4e15b957fff492f3146482bfeecb310
MateusAmaralFranze <amaralmateusdev@gmail.com>
2025-04-14 17:15:41 -0300

    Exercício 1

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+//Exercício 1:
+
+let variavel;
+console.log(variavel)
+
+let variavel2 = null;
+console.log(variavel2)
+
+let variavel3 = 40;
+console.log(variavel3)
+
+
+
+
+
+//Exercício 2:
+
+
+
+//Exercício 3:
+//Exercício 4:
+//Exercício 5:
+//Exercício 6:
+//Exercício 7:
+//Exercício 8:
+//Exercício 9:
